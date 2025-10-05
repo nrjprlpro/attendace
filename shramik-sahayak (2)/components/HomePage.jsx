0bfa6826f7b2ed291649b88c4671c1b791373a25
nrjprlpro <nrj.prl.pro@gmail.com>
2025-10-05 05:45:14 +0530

    Create HomePage.jsx

---
+++ shramik-sahayak (2)/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Your Attendance App</h1>
+      <p>The app is now running correctly!</p>
+    </div>
+  );
+};
+
+export default HomePage;
